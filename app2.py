300a50a00962ff3c980486d35b98d34ea5e4373c
zlaat <158807621+zlaat@users.noreply.github.com>
2025-08-03 10:38:03 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -18,6 +18,5 @@ app.add_url_rule('/', 'home', index)
 
 if __name__ == "__main__":
     # Запускаємо веб-сервер:
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port, debug=False)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
